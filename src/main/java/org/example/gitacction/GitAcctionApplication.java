24fb2e895aa6f277ad72c64d5859d44ffa37b1e9
AhmdFahad <ahmd66699@gmail.com>
2024-02-10 15:36:11 +0300

    create app

---
+++ src/main/java/org/example/gitacction/GitAcctionApplication.java
@@ -0,0 +1,19 @@
+package org.example.gitacction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GitAcctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitAcctionApplication.class, args);
+    }
+    @GetMapping("/")
+    public String hello(){
+        return "hello World";
+    }
+}
---
+++ src/test/java/org/example/gitacction/GitAcctionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gitacction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitAcctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
